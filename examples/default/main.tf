81f452b72dbca2c04a50adc4cd5a95f04a7bcc57
Stephen Rouleau <stephen.rouleau@fxinnovation.com>
2022-03-18 14:04:20 -0400

    Convert dazzlingwrench Gitea URL to FXI Github Enterprise URL

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,12 +1,12 @@
 module "az_rg_demo" {
-  source   = "git::ssh://git@scm.dazzlingwrench.fxinnovation.com:2222/fxinnovation-public/terraform-module-azurerm-resource-group.git?ref=0.2.0"
+  source   = "git::ssh://git@github.com/FXinnovation/fx-terraform-module-azurerm-resource-group.git?ref=0.2.0"
   location = var.location
   name     = var.resource_group_name
   tags     = var.tags
 }
 
 module "az_vnet_demo" {
-  source              = "git::ssh://git@scm.dazzlingwrench.fxinnovation.com:2222/fxinnovation-public/terraform-module-azurerm-virtualnetwork.git?ref=v0.2"
+  source              = "git::ssh://git@github.com/FXinnovation/fx-terraform-module-azurerm-virtualnetwork.git?ref=v0.2"
   resource_group_name = module.az_rg_demo.name
   vnet_name           = var.vnet_name
   vnet_address_space  = var.vnet_address_space
@@ -15,14 +15,14 @@ module "az_vnet_demo" {
 }
 
 module "az_subnets_demo" {
-  source               = "git::ssh://git@scm.dazzlingwrench.fxinnovation.com:2222/fxinnovation-public/terraform-module-azurerm-virtualnetwork-subnet.git?ref=v0.2"
+  source               = "git::ssh://git@github.com/FXinnovation/fx-terraform-module-azurerm-virtualnetwork-subnet.git?ref=v0.2"
   resource_group_name  = module.az_rg_demo.name
   virtual_network_name = module.az_vnet_demo.virtual_network_name
   subnets_config       = var.subnets_config
 }
 
 module "az_nsg_demo" {
-  source                         = "git::ssh://git@scm.dazzlingwrench.fxinnovation.com:2222/fxinnovation-public/terraform-module-azurerm-networksecuritygroup.git?ref=v0.2"
+  source                         = "git::ssh://git@github.com/FXinnovation/fx-terraform-module-azurerm-networksecuritygroup.git?ref=v0.2"
   resource_group_name            = module.az_rg_demo.name
   subnets_config                 = var.subnets_config
   network_security_groups_config = var.network_security_groups_config
